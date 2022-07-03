76039a7f5f7347b3a5d94516356052fa5f82cfce
alwos19 <jhon.jaramilloe@udea.edu.co>
2022-07-03 16:48:43 -0500

    arreglando conflicto de librerias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,8 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
 import { SideBarComponent } from './shared/components/side-bar/side-bar.component';
-<<<<<<< HEAD
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-=======
-import { RouterModule } from '@angular/router';
-import { HttpClientModule } from '@angular/common/http';
->>>>>>> c9aa6773565a0acefaa36fcdc5fb09745181c8f8
 
 @NgModule({
   declarations: [
@@ -24,13 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     HttpClientModule,
     NgbModule
-=======
-    RouterModule,
-    HttpClientModule
->>>>>>> c9aa6773565a0acefaa36fcdc5fb09745181c8f8
   ],
   providers: [],
   bootstrap: [AppComponent]

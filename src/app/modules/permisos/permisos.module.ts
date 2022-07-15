62402c4bd9fa8af22043826e48fbb8d8d7859ac7
Simon3640 <simon.garcial@udea.edu.co>
2022-07-15 18:42:29 -0500

    Merge anlly

--- src/app/modules/permisos/permisos.module.ts
+++ src/app/modules/permisos/permisos.module.ts
@@ -2,25 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { PermisosRoutingModule } from './permisos-routing.module';
-<<<<<<< HEAD
-=======
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CrearPermisoComponent } from './pages/crear-permiso/crear-permiso.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
->>>>>>> anlly
 
 
 @NgModule({
   declarations: [
-<<<<<<< HEAD
-  ],
-  imports: [
-    CommonModule,
-    PermisosRoutingModule
-  ]
-})
-export class PermisosModule { }
-=======
     CrearPermisoComponent
   ],
   imports: [
@@ -33,4 +21,3 @@ export class PermisosModule { }
 })
 export class PermisosModule { }
 
->>>>>>> anlly

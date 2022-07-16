587153c7004c8ff48dd69a667e787534f7235b6d
Simon3640 <simon.garcial@udea.edu.co>
2022-07-16 17:31:40 -0500

    Arreglando commits y demás cosas con la rama Anlly

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -2,17 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { DiasHabiles } from '@shared/clases/dias-habiles';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  template: `<app-tabla-solicitudes></app-tabla-solicitudes>`
 })
 export class HomeComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-
-    DiasHabiles(new Date(2020, 0, 2), new Date(2021, 0, 145));
   }
 
 }

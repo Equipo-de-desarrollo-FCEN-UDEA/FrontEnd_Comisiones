04bc7eee2e921094132f6316615dc77e67746b7c
DavidTorres-sys <david.torresg@udea.edu.co>
2022-08-02 11:39:53 -0500

    prueba 2

--- src/app/shared/data/ruta-api.ts
+++ src/app/shared/data/ruta-api.ts
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-export var prefix: string = 'https://66f1-2800-e2-1a00-f73-cc9b-b75c-7757-2c4d.ngrok.io/api/';
-=======
-export var prefix: string = 'http://localhost:5200/api/';
->>>>>>> origin
+export const prefix: string = 'http://fcen-udea.co/develop/api/usuarios/api/';

e85e14c94808080190f02e6005a74c1654065258
Simon3640 <simon.garcial@udea.edu.co>
2022-08-19 00:41:25 -0500

    error

--- src/app/modules/dexclusiva/pages/editar-dedicacion/editar-dedicacion.component.ts
+++ src/app/modules/dexclusiva/pages/editar-dedicacion/editar-dedicacion.component.ts
@@ -1,7 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute} from '@angular/router';
 import { DedicacionDTO } from '@interfaces/dedicaciones/dedicaciones';
-import { DedicacionService } from '@services/dedicaciones/dedicacion.service';
 import { BehaviorSubject } from 'rxjs';
 import { CrearComisionComponentsService } from '../../services/crear-comision-components.service';
 
@@ -23,7 +22,6 @@ export class EditarDedicacionComponent implements OnInit {
   id$ : BehaviorSubject<Number> = new BehaviorSubject<Number>(0);
 
   constructor(
-    private dedicacionSvc : DedicacionService,
     private comunicacionSvc : CrearComisionComponentsService,
     private activatedRoute : ActivatedRoute
   ) { 
@@ -32,15 +30,7 @@ export class EditarDedicacionComponent implements OnInit {
 
     this.activatedRoute.params.subscribe(params => {
       id = params['id'];
-      this.comunicacionSvc.setId(id);
-      let dedicacion : DedicacionDTO;
-      this.dedicacionSvc.getDedicacion(id).subscribe(
-        (data: DedicacionDTO) => {
-          dedicacion = data;
-          console.log(dedicacion);
-        });
-
-      
+      this.comunicacionSvc.setId(id);      
     });
   }
 
